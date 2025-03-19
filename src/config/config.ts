7144ad9727f429472abca008d615469001bc6d48
DESKTOP-EE7UA1C\aman <amanullahshamsi22@gmail.com>
2025-03-19 14:05:06 +0500

    feat: Project environment setup

---
+++ src/config/config.ts
@@ -0,0 +1,11 @@
+import DotenvFlow from 'dotenv-flow'
+
+DotenvFlow.config() //pick env
+export default {
+    //General
+    ENV: process.env.ENV,
+    PORT: process.env.PORT,
+    SERVER_URL: process.env.SERVER_URL,
+    // Database
+    DATABASE_URL: process.env.DATABASE_URL
+}
